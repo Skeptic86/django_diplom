c84a170259735a884ef617d0f172340d8bcd090f
Омаров Руслан Теймурович <ruslan.omarov773@gmail.com>
2024-01-21 16:39:07 +0500

    made Posts model

--- project/posts/models.py
+++ project/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    theme = models.CharField('Тема', max_length=30)
+    date = models.DateField('Дата и время')
+    is_shkn = models.BooleanField('Шкн в названии')
+    is_link = models.BooleanField('Есть ссылка')
+    appointment = models.CharField('Назначение', max_length=50)
+
+    def __str__(self):
+        return f'${self.theme}:${self.appointment}'
+    
